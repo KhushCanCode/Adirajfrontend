8c6598f1ac1c1e7e708f4518f0f7019f7bedfaba
Khushi <kaybesky89@gmail.com>
2025-02-15 08:58:24 +0530

    login and sign up done

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import loginImage from '../assets/login-image3.jpg'
 
 function LoginPage() {
   return (
-    <div className='mt-28 md:mt-36 h-[50vh] w-full flex items-center justify-center'>
-        This is login page.
+    <div className='mt-10 md:mt-16 w-full flex items-center justify-center h-screen'>
+        <div className="bg-gray-50 flex justify-center items-center w-full">
+
+        <div className="w-1/2 h-screen hidden lg:block">
+          <img src={loginImage} alt="Login Image" className="object-cover object-center w-full h-full"/>
+        </div>
+      <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+        <h1 className="text-2xl font-semibold mb-4 text-dark">Login</h1>
+        <form action="#" method="POST">
+          <div className="mb-4">
+            <label for="username" className="block text-gray-600">Username</label>
+            <input type="text" id="username" name="username" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-secondary" autocomplete="off"/>
+          </div>
+          <div className="mb-4">
+            <label for="password" className="block text-gray-600">Password</label>
+            <input type="password" id="password" name="password" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:secondary" autocomplete="off"/>
+          </div>
+          <div className="mb-4 flex items-center">
+            <input type="checkbox" id="remember" name="remember" className="text-secondary"/>
+            <label for="remember" className="text-gray-600 ml-2">Remember Me</label>
+          </div>
+          <div className="mb-6 text-gray-400">
+            <a href="#" className="hover:underline">Forgot Password?</a>
+          </div>
+          <button type="submit" className="bg-secondary hover:bg-primary text-white font-semibold rounded-md py-2 px-4 w-full cursor-pointer">Login</button>
+        </form>
+        <div className="mt-6 text-primary text-center">
+          <Link to="/register" className="hover:underline">Don't have an account? Sign up</Link>
+        </div>
+      </div>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import loginImage from '../assets/login-image3.jpg'
 
 function RegisterPage() {
   return (
-    <div>RegisterPage</div>
+    <div className='mt-10 md:mt-16 w-full flex items-center justify-center h-screen'>
+    <div className="bg-gray-50 flex justify-center items-center w-full">
+
+    <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2 ">
+    <h1 className="text-2xl font-semibold mb-4 text-dark">Register</h1>
+    <form action="#" method="POST">
+      <div className="mb-4">
+        <label for="username" className="block text-gray-600">Username</label>
+        <input type="text" id="username" name="username" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-secondary" autocomplete="off"/>
+      </div>
+      <div className="mb-4">
+        <label for="email" className="block text-gray-600">Email</label>
+        <input type="email" id="email" name="email" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:secondary" autocomplete="off"/>
+      </div>
+      <div className="mb-4">
+        <label for="password" className="block text-gray-600">Password</label>
+        <input type="password" id="password" name="password" className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:secondary" autocomplete="off"/>
+      </div>
+      <button type="submit" className="bg-secondary hover:bg-primary text-white font-semibold rounded-md py-2 px-4 w-full cursor-pointer">Register</button>
+    </form>
+    <div className="mt-6 text-primary text-center">
+      <Link to="/login" className="hover:underline">Already have an account? Log In</Link>
+    </div>
+  </div>
+
+    <div className="w-1/2 h-screen hidden lg:block">
+      <img src={loginImage} alt="Login Image" className="object-cover object-center w-full h-full"/>
+    </div>
+
+  </div>
+</div>
   )
 }
 
